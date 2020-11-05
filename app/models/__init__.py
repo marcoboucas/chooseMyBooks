697bab48e2f3efc77a8e263ae16f5db9fa73e8fc
Marco Boucas <marco.boucas@hotmail.fr>
2020-11-05 10:37:56 +0100

    remove django app from this repository

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Init."""
-
-from .book import Book
-from .author import Author
-from .reading import Reading
--- app/models/author.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Author(models.Model):
-    """Author model."""
-    name = models.CharField("Name", max_length=100)
-
-    def __str__(self):
-        return self.name
